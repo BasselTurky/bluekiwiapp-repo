587b9925fa76870e66410afd63bac632f521a7c1
BasselTurky <85626321+BasselTurky@users.noreply.github.com>
2023-10-29 11:03:03 +0200

    setting up socket.io

--- socket-image/socket/index.js
+++ socket-image/socket/index.js
@@ -1,19 +1,23 @@
 const http = require("http");
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 // const server = require("http").createServer(app);
 const socketIo = require("socket.io");
 const port = process.env.SERVER_PORT;
 const cors = require("cors");
-// app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" }));
 const pool = require("./database");
 
-const server = http.createServer();
+const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
+});
+
+app.get("/ws", (req, res) => {
+  res.send(`Hello, this is socket.io server running on port ${port}`);
 });
 
 io.on("connection", (socket) => {
