f4f62dae25520964b61bca13a95d4e38260ac75f
BasselTurky <85626321+BasselTurky@users.noreply.github.com>
2024-03-03 16:10:40 +0200

    quick fix

--- auth-image/auth/index.js
+++ auth-image/auth/index.js
@@ -362,7 +362,7 @@ app.post("/auth/sign-google-idToken", async (req, res) => {
     // const userId = payload.sub;
     // const email = payload.email;
     // const name = payload.name;
-
+    console.log(payload);
     const googleid = payload.userId;
     const email = payload.email;
     const name = payload.name;
