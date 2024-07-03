d606e89c3d4591a13de8c2c7f6ed67462d34bbaf
BasselTurky <85626321+BasselTurky@users.noreply.github.com>
2024-07-03 19:00:51 +0300

    up

--- socket-image/socket/databasePromise.js
+++ socket-image/socket/databasePromise.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  connectTimeout: 10000,
 });
 
 // Example function to test connection and query
