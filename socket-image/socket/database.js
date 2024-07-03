25ec088e102b3aad168486990f52285baeade0cf
BasselTurky <85626321+BasselTurky@users.noreply.github.com>
2024-07-03 20:15:58 +0300

    reset database file (working)

--- socket-image/socket/database.js
+++ socket-image/socket/database.js
@@ -15,30 +15,24 @@ const pool = mysql.createPool({
 // DB_NAME=inviteme
 
 pool.getConnection((err, connection) => {
-  try {
-    console.log("here db");
-    if (err) {
-      if (err.code === "PROTOCOL_CONNECTION_LOST") {
-        console.error("Database connection was closed.");
-      }
-      if (err.code === "ER_CON_COUNT_ERROR") {
-        console.error("Database has too many connections.");
-      }
-      if (err.code === "ECONNREFUSED") {
-        console.error("Database connection was refused.");
-      }
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
     }
-    console.log("here4");
-    if (connection) {
-      console.log("Database Connected!");
-      console.log("here2");
-      connection.release();
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
     }
-    console.log("here3");
-    return;
-  } catch (error) {
-    console.log("db err ", error);
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
   }
+
+  if (connection) {
+    console.log("Database Connected!");
+    connection.release();
+  }
+
+  return;
 });
 
 pool.query = util.promisify(pool.query);
