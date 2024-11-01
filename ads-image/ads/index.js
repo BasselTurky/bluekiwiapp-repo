7ba11ce698a7969af1149c2c0774ed6cd641ebaa
BasselTurky <85626321+BasselTurky@users.noreply.github.com>
2024-11-01 06:33:02 +0200

    up

--- ads-image/ads/index.js
+++ ads-image/ads/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Define a route for `app-ads.txt`
 app.get("/app-ads.txt", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/app-ads.txt")); // Update the path accordingly
+  res.sendFile(path.join(__dirname, "public/add-ads.txt")); // Update the path accordingly
 });
 
 const port = process.env.SERVER_PORT;
