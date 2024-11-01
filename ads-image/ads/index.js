275ebbe4be7612d6cae71b90be1071e8899b8a0c
BasselTurky <85626321+BasselTurky@users.noreply.github.com>
2024-11-01 06:24:23 +0200

    create add-ads.txt image

---
+++ ads-image/ads/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Define a route for `app-ads.txt`
+app.get("/app-ads.txt", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/app-ads.txt")); // Update the path accordingly
+});
+
+const port = process.env.SERVER_PORT;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
